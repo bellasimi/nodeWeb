3816bb23acca003b2c9e61cb8ad52717758d5ff9
bellasimi <WorkSY1111@gmail.com>
2021-12-02 01:03:43 +0900

    [Back] node 서버 port 변경 및 react bulid html파일 연동

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 /* express불러오기 */
 const express = require("express");
+const path = require("path");
 
 /*웹서버 생성*/
 const app = express();
-
+const port = 8282
 app.set("views",__dirname+"/views");
 app.set("view engine","pug");
 
@@ -11,4 +12,11 @@ app.get("/",(req,res) => {
     console.log("hello world!");
     res.render("index");
 });
-app.listen(5050,() => console.log("server : http://localhost:5050"));
+
+app.use(express.static(path.join(__dirname,"profile/build")));
+
+app.get("/react",(req,res) => {
+    res.sendFile(path.join(__dirname,"profile/build/index.html"));
+});
+
+app.listen(port,() => console.log("server : http://localhost:8282"));
